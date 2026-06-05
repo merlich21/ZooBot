caf562774bcce9759f584962a001752fd6caff73
AlexandSko <alexandsko@gmail.com>
2026-06-05 21:50:17 +0300

    its done

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
 
-# BOT_TOKEN = os.getenv("BOT_TOKEN", "ваш_токен_от_BotFather")
-# BOT_TOKEN = os.getenv("BOT_TOKEN", "8878357595:AAHQRmCCEF0EA1UFjBfmF0WbJpSGsJ8ZUjk")
-BOT_TOKEN = "8878357595:AAHQRmCCEF0EA1UFjBfmF0WbJpSGsJ8ZUjk"
+BOT_TOKEN = os.getenv("BOT_TOKEN", "ваш_токен_от_BotFather")
 
 ADMIN_CHAT_ID = 999999999  # ID чата сотрудника зоопарка (для пересылки результатов)
